81055942556166a7824ba8e1df1ff2270b8093d2
Paul Goldstein <turboskomorokh@gmail.com>
2024-08-10 03:48:27 +0300

    Добавил пример использования, если не предоставлены аргументы

--- src/emu.cpp
+++ src/emu.cpp
@@ -7,6 +7,9 @@
 using std::fstream, std::ios;
 
 int main(int argc, char **argv) {
+  if(!argv[1]) {
+    printf("Usage: emulator [binary file] [cycles]");
+  }
  Memory mem;
  CPU_6502 cpu;
  cpu.Reset(mem);
@@ -16,8 +19,8 @@ int main(int argc, char **argv) {
 
  cpu.Execute(std::stoi((std::string)argv[2]), mem);
  mem.PrintRange(0x0100, 0x0200);
- mem.PrintRange(0x2000, 0x2020);
- mem.PrintRange(0x8000, 0x8020);
+ mem.PrintRange(0x2000, 0x2100);
+ mem.PrintRange(0x8000, 0x8100);
  mem.PrintRange(0xFFFA, 0xFFFF);
  cpu.PrintFlags();
  cpu.PrintRegisters();
