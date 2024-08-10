d16b104acaab89154efcb56da9ed695440f723b7
Paul Goldstein <turboskomorokh@gmail.com>
2024-08-10 04:00:39 +0300

    Исправлена ошибка использования fopt

--- program/src/program.s
+++ program/src/program.s
@@ -1,25 +1,25 @@
-.fopt	author, "Paul Goldstein"
-.fopt	epoch, "1723251405"
-.fopt	name, "It counts to 10!"
+; Paul Goldstein
+; 1723251405
+; It counts to 10!
 .setcpu  "6502"
 
 .segment "CODE"
 
 START:
-	JSR INX10   ; 6
-	STA $2000   ; 4
-	BRK         ; 7
+	JSR INX10 ; 6 cycles: Jump to INX10, saves return address
+	STA $2000 ; 4 cycles: Store accumulator to memory location $2000
+	BRK       ; 7 cycles: Break, stops execution
 
 INX10:
-	INX         ; 2
-	TXA         ; 2
-	CMP #10     ; 2
-	BEQ INX10END; 2 (+1 on the end)
-	JMP INX10   ; 3
+	INX          ; 2 cycles: Increment accumulator (A)
+	TXA          ; 2 cycles: Transfer accumulator to X register
+	CMP #10      ; 2 cycles: Compare X with 10
+	BEQ INX10END ; 2 cycles (plus 1 cycle if branch taken): Branch to INX10END if X = 10
+	JMP INX10    ; 3 cycles: Jump to INX10 if comparison not equal
 INX10END:
-	RTS         ; 6
+	RTS ; 6 cycles: Return from subroutine
 
 .segment "RESET"
-    .word $FF00 ; NMI
-    .word $8000 ; RESET
-    .word $FF00 ; BRK
+    .word $FF00 ; NMI vector location
+    .word $8000 ; Reset vector location
+    .word $FF00 ; BRK vector location
